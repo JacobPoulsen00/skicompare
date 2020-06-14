a8f29ba03410ba70244302177f00db3afb441a05
Jacob Poulsen <jacobhogstedt@gmail.com>
2020-06-14 09:43:04 +0200

    Updates

---
+++ precache-manifest.9210c23b0e7436e5c4f1c0950ed9176b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3ab49d8becef4e2cc37f2bdff530e6d",
+    "url": "/skicompare/index.html"
+  },
+  {
+    "revision": "7b2665b347339601a3fb",
+    "url": "/skicompare/static/css/main.998b38c3.chunk.css"
+  },
+  {
+    "revision": "1db430a1c049957c8c57",
+    "url": "/skicompare/static/js/2.1c83615b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/skicompare/static/js/2.1c83615b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b2665b347339601a3fb",
+    "url": "/skicompare/static/js/main.c6a575fd.chunk.js"
+  },
+  {
+    "revision": "527a5faa3b857814bf36",
+    "url": "/skicompare/static/js/runtime-main.0ef50d3b.js"
+  },
+  {
+    "revision": "4f50929b18d74f51403f39b0cf734867",
+    "url": "/skicompare/static/media/titleCard.4f50929b.jpeg"
+  }
+]);

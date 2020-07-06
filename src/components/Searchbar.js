1d51b6e9370497cba190ce6987f32c67f288d8aa
Jacob Poulsen <jacobhogstedt@gmail.com>
2020-07-06 13:33:00 +0200

    sb

---
+++ src/components/Searchbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/styles.css';
+
+class Searchbar extends React.Component {
+	render() {
+		return (
+      <div>
+        <p>hey</p>
+      </div>
+        );
+    }
+} 
+export default TitleCard;
